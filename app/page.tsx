1df8e22ab7041bfd1d0fcd90080431917490b3e7
Fayaaz Ahmed <fayaaz.ahmed.20@gmail.com>
2026-01-18 12:36:21 -0500

    Moren cool branding

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
       <main className="w-full max-w-xl p-6">
-        <h1 className="text-4xl sm:text-5xl font-light text-foreground mb-10 text-center">
+        <h1 className="text-4xl sm:text-5xl font-light text-black mb-7 text-center">
           Welcome to Watchdog.
+        </h1>
+        <h1 className="text-l sm:text-l mb-10 text-center text-black">
+          Threat Detection and Mapping for First Responders
         </h1>
         <LoginForm />
       </main>
